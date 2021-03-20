7fcfb63fc8e060fc01863748468f282b55c4b06f
Jana Valášková <jancav@seznam.cz>
2021-03-20 19:48:26 +0100

    domaci ukol

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
 // tady je místo pro náš program
+
+let nadpis = document.querySelector("p");
+
+
+
+function priNajeti(nadpis) {
+    nadpis.target.style.fontWeight="bold";
+}
+function priOdjeti(nadpis) {
+    nadpis.target.style.fontWeight="normal";
+}
+
+
+
+function priKliknuti () {
+    nadpis.classList.toggle("cervenyLev");
+}
+
+function takyPriKliknuti () {
+nadpis.style.fontSize = "17px";
+}
+
+
+
+function prehraj(elementAudio) {
+    let prehrajAudio = document.querySelector(elementAudio);
+    prehrajAudio.play();
+}
+
+function pauza(elementAudio) {
+    let pauzaAudio = document.querySelector(elementAudio);
+    pauzaAudio.pause();
+}
+
+function hlasitostNula(elementAudio) {
+    let hlasitostNulaAudio = document.querySelector(elementAudio);
+    hlasitostNulaAudio.volume = 0;
+}
+
+function hlasitostNaPul(elementAudio) {
+    let hlasitostNaPulAudio = document.querySelector(elementAudio);
+    hlasitostNaPulAudio.volume = 0.5;
+}
+
+function hlasitostRandal(elementAudio) {
+    let hlasitostRandalAudio = document.querySelector(elementAudio);
+    hlasitostRandalAudio.volume = 1;
+}
+
+function naZacatek(elementAudio) {
+    let naZacatekAudio = document.querySelector(elementAudio);
+    naZacatekAudio.currentTime = 0;
+}
